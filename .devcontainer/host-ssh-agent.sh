f1fe0c067e1f99cba443c9dfc6d3510985008f99
Zakhar Skorokhodov <zaharsk@gmail.com>
2026-05-15 21:29:57 +0000

    [devcontainer] Update SSH handeling

---
+++ .devcontainer/host-ssh-agent.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -ueo pipefail
+
+if ! ssh-add -l; then
+    echo "Add new key"
+    ssh-add --apple-use-keychain ~/.ssh/id_ed25519  
+fi
--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -2,6 +2,14 @@
 
 set -ueo pipefail
 
-git config --global user.email "zaharsk@gmail.com"
-git config --global user.name "Zakhar Skorokhodov"
+git config --global user.name "${GIT_USER_NAME}"
+git config --global user.email "${GIT_USER_EMAIL}"
 git config --global pull.rebase true
+
+mkdir -p ~/.ssh
+
+ssh-add -L | head -n1 > ~/.ssh/id_ed25519.pub
+
+git config --global gpg.format ssh
+git config --global user.signingkey ~/.ssh/id_ed25519.pub
+git config --global commit.gpgsign true
