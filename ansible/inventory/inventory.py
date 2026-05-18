65146a015a3f2cc3a80bf973d99cd3b6a2b18a82
Zakhar Skorokhodov <zaharsk@gmail.com>
2026-05-18 12:03:18 +0000

    [ansible] Add basic setup with inventory

---
+++ ansible/inventory/inventory.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+from collections import defaultdict
+import json
+import os
+from pathlib import Path
+import subprocess
+
+env = os.environ.copy()
+
+TERRAFORM_FOLDER = "terraform"
+TF_PATH = Path(f"{env["WORKSPACE_FOLDER"]}/{TERRAFORM_FOLDER}")
+SSH_PRIVATE_KEY_PATH = Path(f"{env["WORKSPACE_FOLDER"]}/{env["SECRETS_FOLDER"]}/ssh.ubuntu")
+
+
+raw = subprocess.check_output(
+    [
+        "terraform",
+        "output",
+        "-json",
+        "inventory",
+    ],
+    cwd=TF_PATH,
+    text=True,
+    env=env,
+)
+
+data = json.loads(raw)
+servers = data["servers"]
+
+inventory = {
+    "_meta": {
+        "hostvars": {}
+    }
+}
+
+groups = defaultdict(list)
+
+for hostname, cfg in servers.items():
+
+    inventory["_meta"]["hostvars"][hostname] = {
+        "ansible_host": cfg["ip"],
+        "ansible_user": cfg["user"],
+        "ansible_ssh_private_key_file": SSH_PRIVATE_KEY_PATH,
+    }
+
+    for group in cfg["groups"]:
+        groups[group].append(hostname)
+
+for group, hosts in groups.items():
+    inventory[group] = {
+        "hosts": hosts
+    }
+
+inventory["all"] = {
+    "children": list(groups.keys())
+}
+
+print(json.dumps(inventory, indent=2, default=str))
