afa77062d52bb61c94eee2931aca74ef3ed48abd
Zakhar Skorokhodov <zaharsk@gmail.com>
2026-05-20 15:44:55 +0000

    [chore] Made ssh-agent usage safe

--- scripts/post-create-command.sh
+++ scripts/post-create-command.sh
@@ -8,12 +8,17 @@ git config --global user.email "${GIT_USER_EMAIL}"
 git config --global pull.rebase true
 git config --global core.autocrlf input
 
-mkdir -p ~/.ssh
+if ssh-add -L >/dev/null 2>&1; then
+    mkdir -p ~/.ssh
 
-ssh-add -L | head -n1 > ~/.ssh/id_ed25519.pub
+    ssh-add -L | head -n1 > ~/.ssh/id_ed25519.pub
 
-git config --global gpg.format ssh
-git config --global user.signingkey ~/.ssh/id_ed25519.pub
-git config --global commit.gpgsign true
+    git config --global gpg.format ssh
+    git config --global user.signingkey ~/.ssh/id_ed25519.pub
+    git config --global commit.gpgsign true
+else
+    echo "SSH agent is unavailable or no identities are loaded."
+    echo "Git commit signing was skipped."
+fi
 
 ./scripts/decrypt_secrets.sh
